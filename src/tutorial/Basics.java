72157d7aa0bce49f99bf4a12bfc51ee381a3f43e
arthur.picerna <arthur.picerna@A19-DAP-339.aviatur.com.co>
2017-07-12 12:12:44 -0500

    refactoring, POO class

--- src/tutorial/Basics.java
+++ src/tutorial/Basics.java
@@ -1,17 +1,16 @@
-package condicionales;
+package tutorial;
 
 import java.util.Scanner;
 import javax.swing.*;
 
-public class Condicionales {
+public class Basics {
 
     public static void main(String[] args) {
 
 //        Scanner keyboard = new Scanner(System.in);
 //        System.out.println("Execute:\n\t1. edad\n\t2. anidado\n\t3. switchmenu");
 //        String choice = keyboard.nextLine();
-
-        String choice = JOptionPane.showInputDialog(null, "Execute:\n1. edad\n2. anidado\n3. switchmenu");
+        String choice = JOptionPane.showInputDialog(null, "Execute:\n1. edad\n2. anidado\n3. switchmenu\n...");
 
         // java <6
 //        if (option.equals("anidado")) {
@@ -33,6 +32,11 @@ public class Condicionales {
             case "restaurant":
                 restaurant();
                 break;
+            case "looping":
+                looping();
+                break;
+            case "looping2":
+                looping2();
             case "catcherror":
                 catcherror();
                 break;
@@ -108,6 +112,40 @@ public class Condicionales {
         }
     }
 
+    private static void looping() {
+        Scanner kb = new Scanner(System.in);
+
+        System.out.print("a:");
+        int a = kb.nextInt();
+
+        System.out.print("b:");
+        int b = kb.nextInt();
+
+        while (a < b) {
+            a++;
+            System.out.println(a);
+        }
+    }
+
+    private static void looping2() {
+        int choice;
+        int a = Integer.parseInt(JOptionPane.showInputDialog(null, "a:"));
+        int b = Integer.parseInt(JOptionPane.showInputDialog(null, "b:"));
+        do {
+            choice = Integer.parseInt(JOptionPane.showInputDialog(null, "1 = suma\n2 = resta\n0 = salir"));
+        } while (choice != 0 && choice != 1 && choice != 2);
+        switch (choice) {
+            case 0:
+                return;
+            case 1:
+                JOptionPane.showMessageDialog(null, a + " + " + b + " = " + (a + b));
+                break;
+            case 2:
+                JOptionPane.showMessageDialog(null, a + " - " + b + " = " + (a - b));
+                break;
+        }
+    }
+
     private static void catcherror() {
         try {
             int i = 0;
@@ -129,14 +167,14 @@ public class Condicionales {
             int n;
             do {
                 n = Integer.parseInt(JOptionPane.showInputDialog(null, "1. sumar\n2. restar\n0. salir"));
-                switch(n) {
+                switch (n) {
                     case 0:
                         break;
                     case 1:
-                        JOptionPane.showMessageDialog(null,"a+b=" + (a+b));
+                        JOptionPane.showMessageDialog(null, "a+b=" + (a + b));
                         break;
                     case 2:
-                        JOptionPane.showMessageDialog(null,"a-b=" + (a-b));
+                        JOptionPane.showMessageDialog(null, "a-b=" + (a - b));
                         break;
                 }
             } while (n != 0);
